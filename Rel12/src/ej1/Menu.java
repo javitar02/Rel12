49d061034c872dcf7aa48c96837f3a5179fed307
JAVI <JAVI@DESKTOP-RBJK8LG>
2021-05-10 10:26:16 +0200

    a

--- Rel12/src/ej1/Menu.java
+++ Rel12/src/ej1/Menu.java
@@ -1,158 +1,156 @@
 package ej1;
-
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
+
 public class Menu {
-private static final int OPCION_SALIR = 5;
-	
+
 	private static Scanner teclado=new Scanner(System.in);
-	public static void main(String[] args) throws IOException {
-		
+	
+	public static void main(String[] args) {
+
 		int opcion;
-		
-		
-		do{
+		do
+		{
 			opcion=mostrarMenu();
 			tratarMenu(opcion);
-		}while (opcion!=OPCION_SALIR);
+		}while (opcion!=5);
 
 	}
-	
-	private static void tratarMenu(int opc) throws IOException {
-		String nomDirectorio;
-		String nomCarpeta;
-		String nomFichero;
-		String nomFicheroABorrar;
-		String contenidoFichero;
-		File contenidoFiles[];
+
+	private static void tratarMenu(int opc) {
+		switch (opc)
+		{
+		case 1:
+		{
+			crearDirectorio();
+			break;
+		}
+		case 2:
+		{
+			crearFichero();
+			break;
+		}
+		case 3:
+		{
+			borrarFichero();
+			break;
+		}
+		case 4:{
+			mostrarLosFicherosDeUnaCarpetaOrdenados();
+			break;
+		}
 		
-		File fichero;
-		File directorio;
+		}
 		
-		boolean creada;
-		boolean borrado;
+	}
+
+	private static void mostrarLosFicherosDeUnaCarpetaOrdenados() {
+		String nombreCarpeta;
+		System.out.println("Introduce el nombre de la carpeta");
+		nombreCarpeta=teclado.nextLine();
+		File[] contenido;
+		ArrayList<String> ficherosQueContiene=new ArrayList<String>();
 		
-			switch (opc){
-			case 1:
-				nomDirectorio=solicitarNombreFichero();
-				directorio=new File(nomDirectorio);
-				
-				if(directorio.isDirectory()) {
-					System.out.println("Carpeta ya existente");
-				}else {
-					creada=directorio.mkdirs();
-					if(creada) {
-						System.out.println("Carpeta creada exitosamente");
-					}
-					System.out.println("Error, carpeta no creada");
-				}
+		File enlaceCarpeta= new File (nombreCarpeta);
+		if ( !enlaceCarpeta.exists() || !enlaceCarpeta.isDirectory()) {
+			System.out.println("No existe o no es una carpeta");
+		}
+		else {
 			
-				break;
-			case 2:
-				nomFichero=solicitarNombreFichero();
-				contenidoFichero=llenarFichero();
-				
-				fichero=new File(nomFichero,contenidoFichero);
-				
-				if(fichero.isFile()) {
-					System.out.println("Fichero de texto ya existente");
+			contenido= enlaceCarpeta.listFiles();
+			
+			for (int i = 0; i < contenido.length; i++) {
+				if (contenido[i].isFile()) {
+					ficherosQueContiene.add(contenido[i].getName());
 				}
-				
-				fichero.createNewFile();
-				System.out.println("Fichero creado correctamente");
-				
-				break;
-			case 3:
-				nomFicheroABorrar=solicitarNombreFichero();
-				File aBorrar=new File(nomFicheroABorrar);
-				
-				borrado=aBorrar.delete();
-				
-				if(borrado) {
-					System.out.println("Fichero borrado correctamente");
-				}else {
-					System.out.println("Error, no se pudo borrar el fichero");
-				}
-				
-				break;
-				
-			case 4:
-				nomCarpeta=solicitarNombreFichero();
-				File carpetaCreada=new File(nomCarpeta);
-				
-				
-				
-				contenidoFiles=carpetaCreada.listFiles();
-				
-				for (int i = 0; i < contenidoFiles.length; i++) {
-					if (contenidoFiles[i].isFile()) {
-						System.out.println(contenidoFiles[i]);
-					}
-				}
-				
-				break;
-				
-			case 5:
-				System.out.println("Has seleccionado salir del menu. Tenga un buen dia");
 			}
+			Collections.sort(ficherosQueContiene);
+			for (String fichero : ficherosQueContiene) {
+				System.out.println(fichero);
+			}
+			
+			
+		}
+	}
+
+	private static void borrarFichero() {
+		String nombreFichero;
+		System.out.println("Introduce el nombre del fichero a borrar");
+		nombreFichero=teclado.nextLine();
+		File enlaceFichero= new File (nombreFichero);
+		if (!(enlaceFichero.exists() && enlaceFichero.isFile()))
+			System.out.println("Error. No existe un fichero con ese nombre.");
+		else
+		{
+			if (enlaceFichero.delete())
+				System.out.println("Fichero " + nombreFichero + " borrado corectamente");
+			else
+				System.out.println("No se ha podido borrar el fichero");
+		}
 		
+	}
+
+	private static void crearFichero() {
+		String nombreFichero, contenido;
+		System.out.println("Introduce el nombre del fichero");
+		nombreFichero=teclado.nextLine();
+		
+		
+		
+		File enlaceFichero= new File (nombreFichero);
+		if ( enlaceFichero.exists() && enlaceFichero.isFile())
+			System.out.println("Error. Ya existe un fichero con ese nombre.");
+		else
+		{
+			System.out.println("Introduce una cadena de texto que ser� el contenido del fichero: ");
+			contenido=teclado.nextLine();
+			
+			try (FileWriter flujoEscritura=new FileWriter(nombreFichero);
+					PrintWriter filtroEscritura=new PrintWriter(flujoEscritura)){
+				filtroEscritura.print(contenido);
+				System.out.println("Fichero creado correctamente");
+			}
+			catch (IOException e)
+			{
+				System.out.println(e.getMessage());
+			}
+			
+		}
+		
+	}
+
+	private static void crearDirectorio() {
+		
+		String nombreCarpeta;
+		boolean creado;
+		System.out.println("Introduce el nombre del directorio");
+		nombreCarpeta=teclado.nextLine();
+		
+		File enlaceCarpeta= new File (nombreCarpeta);
+		
+		if ( enlaceCarpeta.exists() && enlaceCarpeta.isDirectory())
+			System.out.println("Error. Ya existe un directorio con ese nombre");
+		else
+		{
+			creado=enlaceCarpeta.mkdir();
+			if ( creado)
+				System.out.println("Directorio creado correctamete");
+			else
+				System.out.println("No se pudo crear el directorio " + nombreCarpeta);
+		}
 	}
 
 	private static int mostrarMenu() {
-		
-		int opcion;
-		
-		do{
-			System.out.println("--BIENVENIDO AL MENU--");
-			System.out.println("1. Crear directorio");
-			System.out.println("2. Crear fichero texto");
-			System.out.println("3. Borrar fichero texto");
-			System.out.println("4. Mostrar ficheros que contiene la carpeta");
-			System.out.println("5. Salir");
-			opcion=introducirNumeroEntero("Introduzca la opcion:");
-		}while (opcion<=0 || opcion> OPCION_SALIR);
-		
-		return opcion;
+		int opc;
+		System.out.println("1.Crear directorio\n2.Crear fichero\n3.Borrar fichero\n4.Mostrar los ficheros que contiene una carperta\n5.Salir");
+		opc=Integer.parseInt(teclado.nextLine());
+		return opc;
 	}
-	
-	private static int introducirNumeroEntero(String enunciado) {
-		
-		int numero=0;
-		boolean hayFallo;
-		
-		do{
-			try{
-				System.out.print(enunciado);
-				numero=Integer.parseInt(teclado.nextLine());
-				hayFallo=false;
-			}catch (NumberFormatException ex){
-				hayFallo=true;
-				System.out.println("ERROR. Solo puedes introducir numeros");
-			}
-		}while (hayFallo);
-		
-		return numero;
-	}
-	
-	public static String solicitarNombreFichero() {
-		String carpeta;
-		
-		System.out.println("Introduce el nombre del fichero o de la carpeta a crear: ");
-		carpeta=teclado.nextLine();
-		
-		return carpeta;
-	}
-	
-	public static String llenarFichero() {
-		String rellenoFichero;
-		
-		System.out.println("Introduce una cadena para rellenar el fichero: ");
-		rellenoFichero=teclado.nextLine();
-		
-		return rellenoFichero;
-	}
-	
-	
+
 }
---
+++ Rel12/src/ej3/ExpresionesRegulares.java
@@ -0,0 +1,81 @@
+package ej3;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+import java.util.Scanner;
+
+public class ExpresionesRegulares {
+	
+	//Suponemos que admitimos mayúsculas solo
+	private static final String PATRON_LINEA = "([A-Z]{2,}[ ]){3}[1-9][0-9]?";
+	private static Scanner teclado=new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		String nombreFichero;
+		boolean correcto;
+		System.out.println("Introduce el nombre del fichero: ");
+		nombreFichero= teclado.nextLine();
+		
+		try {
+			correcto=comprobarSiCumpleFormato(nombreFichero);
+			if (correcto) {
+				System.out.println("El formato del fichero es correcto");
+			}
+			else {
+				System.out.println("El formato del fichero no es correcto");
+			}
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+		}
+		
+		
+	}
+
+	private static boolean comprobarSiCumpleFormato(String nombreFichero) throws IOException {
+	
+		String linea;
+		boolean correcto=true;
+	
+		try (BufferedReader filtroLectura=new BufferedReader(new FileReader(nombreFichero));)
+		{
+
+			linea=filtroLectura.readLine();
+			while ( linea!=null && correcto){
+				correcto= comprobarLineaCorrecta(linea);
+				
+				if (!correcto) {
+					System.out.println("Error en la linea " + linea);
+				}
+				else {
+					linea=filtroLectura.readLine();
+				}
+			}
+			
+			
+		}
+		catch(FileNotFoundException e){
+			System.out.println("No existe el fichero " + nombreFichero);
+		}
+		
+		
+		return correcto;
+		
+	}
+
+	private static boolean comprobarLineaCorrecta(String linea) {
+		
+
+		return linea.matches(PATRON_LINEA );
+	}
+
+	
+		
+		
+	
+
+}
+
